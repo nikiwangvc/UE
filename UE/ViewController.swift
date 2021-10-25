8a33c6071980b03bfece118307db4855b054c33d
nikiwangvc <75057474+nikiwangvc@users.noreply.github.com>
2021-10-25 15:19:04 -0400

    orange theme

--- UE/ViewController.swift
+++ UE/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController{
         createButton()
     }
     func createButton(){
-        let button = UIButton(frame:CGRect(x: 0, y: 0, width: 200, height: 200))
+        let button = UIButton(frame:CGRect(x: 0, y: 0, width: 150, height: 150))
         button.center = view.center
         button.configuration = .RecordButton()
         view.addSubview(button)
@@ -22,8 +22,8 @@ class ViewController: UIViewController{
 //using extension UIButton to add more buttons in the future
 extension UIButton.Configuration{
     static func RecordButton() -> UIButton.Configuration{
-        var config: UIButton.Configuration = .tinted()
-        config.baseBackgroundColor = .systemGreen
+        var config: UIButton.Configuration = .filled()
+        config.baseBackgroundColor = .systemOrange
         config.title = "Start"
         config.subtitle = "Recording"
         config.cornerStyle = .large
