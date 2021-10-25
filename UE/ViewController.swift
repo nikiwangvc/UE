0be0b73c670540dfeaa5f243bf5b5f3439c760c5
nikiwangvc <75057474+nikiwangvc@users.noreply.github.com>
2021-10-25 14:31:08 -0400

    first commit new project

--- UE/ViewController.swift
+++ UE/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController{
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createButton()
     }
-
-
+    func createButton(){
+        let button = UIButton(frame:CGRect(x: 0, y: 0, width: 200, height: 500))
+        button.center = view.center
+        if #available(iOS 15.0, *) {
+            button.configuration = createConfig()
+        } else {
+            // Fallback on earlier versions
+        }
+        view.addSubview(button)
+    }
+    
+    @available(iOS 15.0, *)
+    func createConfig() -> UIButton.Configuration{
+        let config: UIButton.Configuration = .filled()
+        
+        return config
+    }
 }
-
