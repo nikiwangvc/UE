9d48e2415a879415b78477d263d67bb841564843
nikiwangvc <75057474+nikiwangvc@users.noreply.github.com>
2021-11-16 17:52:27 -0500

    login + signup firebase worked!

--- UE/NewSignUpViewController.swift
+++ UE/NewSignUpViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class NewSignUpViewController: UIViewController {
 
@@ -19,6 +21,39 @@ class NewSignUpViewController: UIViewController {
     }
     
     @IBAction func signUpTapped(_ sender: Any) {
+        if email.text?.isEmpty == true {
+            print("Please enter your email.")
+            // Create new Alert
+            let dialogMessage = UIAlertController(title: "Attention", message: "Please enter your email", preferredStyle: .alert)
+            
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+             })
+            
+            //Add OK button to a dialog message
+            dialogMessage.addAction(ok)
+            // Present Alert to
+            self.present(dialogMessage, animated: true, completion: nil)
+            return
+        }
+        if password.text?.isEmpty == true {
+            print("Please enter your password.")
+            // Create new Alert
+            let dialogMessage = UIAlertController(title: "Attention", message: "Please enter your password", preferredStyle: .alert)
+            
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+             })
+            
+            //Add OK button to a dialog message
+            dialogMessage.addAction(ok)
+            // Present Alert to
+            self.present(dialogMessage, animated: true, completion: nil)
+            return
+        }
+        signUp()
     }
     
     
@@ -29,4 +64,16 @@ class NewSignUpViewController: UIViewController {
         present(vc,animated:true)
     }
     
+    func signUp(){
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (authResult,error)in
+            guard let users = authResult?.user,error == nil else{
+                print("Error \(error?.localizedDescription)")
+                return
+            }
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "RecordTripsViewController")
+            vc?.modalPresentationStyle = .overFullScreen
+            self.present(vc!,animated:true)
+        }
+    }
 }
