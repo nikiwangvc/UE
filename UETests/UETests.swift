97ebb0a0164c630e71c8ba33e78cfafa23af9402
Jeffrey Kozik <jeffreykozik@protonmail.com>
2021-11-17 16:38:00 -0500

    test recordtripviewcontroller class

--- UETests/UETests.swift
+++ UETests/UETests.swift
@@ -18,16 +18,28 @@ class UETests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testThatDistanceUpdatesWhenStartPressed() throws {
+    func testRecordTripsViewController() throws {
         // https://stackoverflow.com/questions/53756455/how-to-write-unit-test-for-button-tap
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let RecordTripsViewController = mainStoryboard   .instantiateViewController(withIdentifier: "RecordTripsViewController") as! RecordTripsViewController
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        let RecordTripsViewController = mainStoryBoard   .instantiateViewController(withIdentifier: "RecordTripsViewController") as! RecordTripsViewController
         RecordTripsViewController.loadViewIfNeeded()
-        RecordTripsViewController.RecordTrip(self)
-//        RecordTripsViewController.RecordTripButton?.sendActions(for: .touchUpInside)
+        let window = UIWindow()
+        window.rootViewController = RecordTripsViewController
+        print("rootViewController \(UIApplication.shared.keyWindow?.rootViewController)")
+        window.makeKeyAndVisible()
+        RecordTripsViewController.view.layoutIfNeeded()
+        // https://developer.apple.com/forums/thread/107239
+        RecordTripsViewController.RecordTrip(self) // start recording trip
         // https://stackoverflow.com/questions/40073738/how-to-get-the-current-title-of-a-button-in-swift-3-0-ios-using-sender-titlefo
         XCTAssertEqual(RecordTripsViewController.RecordTripButton.currentTitle!, "Stop Trip")
-//        XCTAssertTrue(true)
+        RecordTripsViewController.RecordTrip(self) // stop recording trip
+        XCTAssertEqual(RecordTripsViewController.RecordTripButton.currentTitle!, "Start Trip")
+        RecordTripsViewController.Friends(self)
+        print("rootViewController \(UIApplication.shared.keyWindow?.rootViewController)")
+        print("Presented \(RecordTripsViewController.presentedViewController!.restorationIdentifier)")
+//        print("Presenting \(RecordTripsViewController.presentingViewController)")
+        print("rootViewController \(UIApplication.shared.keyWindow?.rootViewController)")
+        XCTAssertEqual(RecordTripsViewController.presentedViewController!.restorationIdentifier, "FriendsViewController")
     }
 
     func testPerformanceExample() throws {
