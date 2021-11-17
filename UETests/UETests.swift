cfb5ef9fb7bd4bbfab8b7b42488367b01fb864e7
nikiwangvc <75057474+nikiwangvc@users.noreply.github.com>
2021-11-17 13:13:05 -0500

    Unit testing

--- UETests/UETests.swift
+++ UETests/UETests.swift
@@ -16,7 +16,9 @@ class UETests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
+    
 
     func testThatDistanceUpdatesWhenStartPressed() throws {
         // This is an example of a functional test case.
